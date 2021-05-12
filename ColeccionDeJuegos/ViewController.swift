d64aefa5c3cec7b69a94f9f7ef6db7d23ef7418f
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-05-12 14:25:11 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Mac 05 on 5/12/21.
+//  Copyright © 2021 Mac 05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
